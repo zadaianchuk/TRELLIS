0c01f6bfaa8d53fa8e0c4a0f6cad7bc0d3b7c528
ForeverFancy <1026816510@qq.com>
2025-11-04 20:39:30 -0800

    Add aesthetic score processing to dataset toolkits

--- dataset_toolkits/setup.sh
+++ dataset_toolkits/setup.sh
@@ -1,1 +1,1 @@
-pip install pillow imageio imageio-ffmpeg tqdm easydict opencv-python-headless pandas open3d objaverse huggingface_hub
+pip install pillow imageio imageio-ffmpeg tqdm easydict opencv-python-headless pandas open3d objaverse huggingface_hub open_clip_torch
